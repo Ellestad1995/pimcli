6987f41ae0aea1b04436d1c7ff710e412e15760b
Joakim Ellestad <joakim.ellestad@gmail.com>
2021-02-09 21:42:49 +0100

    tests fro role assignments

---
+++ Public/Get-PrivilegedRoleAssignments.Tests.ps1
@@ -0,0 +1,23 @@
+
+BeforeAll {
+    $Path = Resolve-Path (Join-Path -Path $PSScriptRoot '..')
+    . $Path\Private\logdate.ps1
+    . $PSScriptRoot\Connect-PIM.ps1
+    . $PSScriptRoot\Role.ps1
+    . $PSScriptRoot\Get-PrivilegedRoleAssignments.ps1  
+}
+Describe 'Get-PrivilegedRoleAssignments' {
+    Context 'Eligible role assignments' {
+
+        BeforeEach {
+            Connect-PIM -Silent -PassThru
+        }
+
+        It 'Get Eligible roles' {
+            $MockRoleAssignmentRequest = Get-Content -Path $Path\Get-PrivilegedRoleAssignments.Tests.json | ConvertFrom-Json
+            Get-PrivilegedRoleAssignments -Eligible -PassThru | ConvertTo-Json | should -Be ($MockRoleAssignmentRequest | ConvertTo-Json)
+        }
+
+      
+    }
+}
